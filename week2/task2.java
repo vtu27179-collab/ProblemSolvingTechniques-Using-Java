660b6e4a2ff561e823be2c53d5d6a7d679fb1675
Himavarshitha <vtu27179@veltech.edu.in>
2026-02-11 14:26:38 +0530

    Create task2.java

---
+++ week2/task2.java
@@ -0,0 +1,47 @@
+// Check odd
+    static PerformOperation isOdd() {
+        return n -> n % 2 != 0;
+    }
+
+    // Check prime
+    static PerformOperation isPrime() {
+        return n -> {
+            if (n <= 1) return false;
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) return false;
+            }
+            return true;
+        };
+    }
+
+    // Check palindrome
+    static PerformOperation isPalindrome() {
+        return n -> {
+            int temp = n, rev = 0;
+            while (temp > 0) {
+                rev = rev * 10 + temp % 10;
+                temp /= 10;
+            }
+            return rev == n;
+        };
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int choice = sc.nextInt();
+            int num = sc.nextInt();
+
+            if (choice == 1)
+                System.out.println(isOdd().check(num) ? "ODD" : "EVEN");
+            else if (choice == 2)
+                System.out.println(isPrime().check(num) ? "PRIME" : "COMPOSITE");
+            else if (choice == 3)
+                System.out.println(isPalindrome().check(num) ? "PALINDROME" : "NOT PALINDROME");
+        }
+
+        sc.close();
+    }
+}
